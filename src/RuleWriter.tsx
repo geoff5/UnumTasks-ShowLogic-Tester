fb36cf53a4e03464ee438e4b9dea74160a967780
Geoff Atkinson <gatkinson@unum.com>
2023-03-23 10:20:43 -0400

    Further POC logic

--- src/RuleWriter.tsx
+++ src/RuleWriter.tsx
@@ -1,22 +1,90 @@
+import React from 'react';
+import { rulesMap } from './maps';
+import { DeliveryClient } from '@kontent-ai/delivery-sdk';
 import { FC, useEffect } from 'react';
 
-const SelectOption = () => { 
+const selectOptionRule = () => { 
   return (
-  <select name="rule">
-    <option value="is">Is</option>
-    <option value="is-not">Is not</option>
-    <option value="has-value">Has value</option>
-  </select>);
+    <>
+      <option value="is">Is</option>
+      <option value="isNot">Is not</option>
+      <option value="hasValue">Has value</option>
+    </>
+  );
 };
 
+const selectOptionField = (response: any) => {
+  if (response && response.items) {
+    return(
+      <>
+        <option value="default" selected>Default</option>
+        {response.items.map((i: any) => (
+          <option value={i.elements.label.value}>{i.elements.label.value}</option>
+        ))}
+      </>
+    )
+  }
+}
+
+const getFormElements = async (context: any, setResponse: Function) => {
+  const client = new DeliveryClient({projectId: context.projectId});
+  const response = await client.items().type('form_element').toPromise();
+
+  if (response && response.data) {
+    setResponse(response.data);
+  }
+}
+
+interface Rule {
+  field?: string;
+  rule?: string;
+  value?: string;
+}
+
 export const RuleWriter: FC = () => {
+  const [response, setResponse]: any = React.useState(null);
+  const [rules, setRules] = React.useState<Array<Rule>>([]);
+  const [elementDisabled, setElementDisabled] = React.useState<boolean>(false);
+
+  useEffect(() => {
+    CustomElement.setValue(JSON.stringify(rules));
+  }, [rules]);
+
   useEffect(() => {
     CustomElement.init((element, context) => {
       console.log('element = ' + element + ' context = ' + context);
       CustomElement.setHeight(350);
       CustomElement.onDisabledChanged(() => console.log(element.disabled));
+      setElementDisabled(element.disabled);
+
+      getFormElements(context, setResponse);
     });
   }, []);
+
+  const setFieldAndCreateRules = (e: any) => {
+    const currentRules = {...rules};
+    const name = e.target.name;
+    const eventValue: string = e.target.value;
+    if(!currentRules[0]) {
+      currentRules[0] = {};
+    }
+
+    switch(name) {
+      case 'field':
+        currentRules[0].field = eventValue;
+        break;
+
+      case 'value':
+        currentRules[0].value = eventValue;
+        break;
+
+      case 'rule':
+        currentRules[0].rule = rulesMap[eventValue];
+        break;
+    }
+    
+    setRules(currentRules);
+  };
 
   return (
     <div style={{ padding: 14 }}>
@@ -34,13 +102,16 @@ export const RuleWriter: FC = () => {
             <td>
               N/A
             </td>
-            <td><input type="text" name="field" /></td>
-            <td>{SelectOption()}</td>
-            <td><input type="text" name="value" /></td>
+            <td>
+              <select disabled={elementDisabled} defaultValue="Select a field" name="field" onChange={(e) => setFieldAndCreateRules(e)}>{selectOptionField(response)}</select>
+            </td>
+            <td><select disabled={elementDisabled} name="rule" onChange={(e) => setFieldAndCreateRules(e)}>{selectOptionRule()}</select></td>
+            <td><input disabled={elementDisabled} type="text" name="value" onChange={(e) => setFieldAndCreateRules(e)} /></td>
             <td><button>X</button></td>
           </tr>
         </table>
       </div>
+      <div>{JSON.stringify(rules)}</div>
     </div>
   );
 };
---
+++ src/maps.tsx
@@ -0,0 +1,5 @@
+export const rulesMap: { [index: string]: string } = {
+    is: "fieldValueIs",
+    isNot: "fieldValueIsNot",
+    hasValue: "fieldValueIsDefined"
+};
