8f23135e5e7a10803dfac59bdbe4b37968eae6ac
Geoff Atkinson <gatkinson@unum.com>
2023-03-29 06:58:56 -0400

    working

--- src/RulesForm.tsx
+++ src/RulesForm.tsx
@@ -1,87 +1,66 @@
-import React from "react";
+import React, { FC, useEffect } from "react";
 import { rulesMap } from "./maps";
 import { DeliveryClient } from "@kontent-ai/delivery-sdk";
-import { FC, useEffect } from "react";
 import { Formik, Field, Form, FieldArray } from "formik";
+import styled from "styled-components";
 
-const selectOptionRule = (initialValue: Array<Rule>) => {
-  return (
-    <>
-      <option
-        value="default"
-        selected={!initialValue[0] || !initialValue[0].rule}
-      >
-        Default
-      </option>
-      <option
-        value="is"
-        selected={
-          initialValue[0] !== undefined &&
-          initialValue[0].rule !== undefined &&
-          initialValue[0].rule === rulesMap["is"]
-        }
-      >
-        Is
-      </option>
-      <option
-        value="isNot"
-        selected={
-          initialValue[0] !== undefined &&
-          initialValue[0].rule !== undefined &&
-          initialValue[0].rule === rulesMap["isNot"]
-        }
-      >
-        Is not
-      </option>
-      <option
-        value="hasValue"
-        selected={
-          initialValue[0] !== undefined &&
-          initialValue[0].rule !== undefined &&
-          initialValue[0].rule === rulesMap["hasValue"]
-        }
-      >
-        Has value
-      </option>
-    </>
-  );
-};
+const RuleRow = styled.div`
+  display: flex;
+`;
 
-const selectOptionField = (response: any, initialValue: Array<Rule>) => {
-  if (response && response.items) {
+const RuleContainer = styled.div`
+  margin: 16px;
+  padding: 8px;
+`;
+const selectOptionRule = ({
+  field,
+  form: { touched, errors },
+  ...props
+}: any) => (
+  <select required {...field}>
+    <option value="" disabled selected>Default</option>
+    <option value="fieldValueIs">Is</option>
+    <option value="fieldValueIsNot">Is not</option>
+    <option value="fieldValueDefined">Has value</option>
+  </select>
+);
+
+const selectOptionField = ({
+  field,
+  form: { touched, errors },
+  ...props
+}: any) => {
+  if (props.response && props.response.items) {
     return (
-      <>
-        <option
-          value="default"
-          selected={!initialValue[0] || !initialValue[0].field}
-        >
-          Default
-        </option>
-        {response.items.map((i: any, index: number) => (
-          <option
-            value={i.system.codename}
-            key={index}
-            selected={
-              initialValue[0] !== undefined &&
-              initialValue[0].field !== undefined &&
-              initialValue[0].field === i.system.codename
-            }
-          >
+      <select required {...field}>
+        <option value="" disabled selected>Default</option>
+        {props.response.items.map((i: any, index: number) => (
+          <option value={i.system.codename} key={index}>
             {i.system.codename}
           </option>
         ))}
-      </>
+      </select>
     );
   }
 };
 
-const getFormElements = async (context: any, setResponse: Function) => {
+const getFormElements = async (
+  context: any,
+  setResponse: Function,
+  setLoading: Function
+) => {
   const client = new DeliveryClient({ projectId: context.projectId });
   const response = await client.items().type("form_element").toPromise();
 
   if (response && response.data) {
     setResponse(response.data);
+    setLoading(false);
   }
+};
+
+const saveRules = (formValues: Rule[], setRules: Function) => {
+  CustomElement.setValue(JSON.stringify(formValues));
+  setRules(formValues);
 };
 
 interface Rule {
@@ -95,25 +74,25 @@ export const RulesForm: FC = () => {
   const [rules, setRules] = React.useState<Array<Rule>>([]);
   const [elementDisabled, setElementDisabled] = React.useState<boolean>(false);
   const [initialValue, setInitialValue] = React.useState<Array<Rule>>([]);
-
-  useEffect(() => {
-    CustomElement.setValue(JSON.stringify(rules));
-  }, [rules]);
+  const [loading, setLoading] = React.useState<boolean>(true);
 
   useEffect(() => {
     CustomElement.init((element, context) => {
       console.log("element = " + element + " context = " + context);
-      CustomElement.setHeight(350);
+      CustomElement.setHeight(600);
       CustomElement.onDisabledChanged(() => console.log(element.disabled));
       setElementDisabled(element.disabled);
       setInitialValue(JSON.parse(element.value));
-
-      getFormElements(context, setResponse);
+      getFormElements(context, setResponse, setLoading);
     });
   }, []);
 
-  const saveRules = (formValues: Rule[]) => {
-    setRules(formValues);
+  const initialValues = {
+    rules: initialValue,
+  };
+
+  if (loading) {
+    return <></>;
   }
 
   return (
@@ -121,50 +100,48 @@ export const RulesForm: FC = () => {
       <h1>This is a test for Show/Hide Logic</h1>
       <div style={{ marginTop: 100 }}>
         <Formik
-          initialValues={initialValue}
-          onSubmit={(values) => saveRules(values)}
+          initialValues={initialValues}
+          onSubmit={(values) => saveRules(values.rules, setRules)}
         >
           {({ values }) => (
             <Form>
               <FieldArray name="rules">
-                {({ remove, push }) => (
+                {({ insert, remove, push }) => (
                   <div>
-                    {values.length > 0 &&
-                      values.map((rule, index) => (
-                        <div className="row" key={`rule-${index}`}>
-                          <div className="col">
-                            <label htmlFor={`rules.${index}.field`}>Field</label>
+                    {values.rules.length > 0 &&
+                      values.rules.map((rule: Rule, index: number) => (
+                        <RuleRow key={`rule-${index}`}>
+                          <RuleContainer>
+                            <label htmlFor={`rules.${index}.field`}>
+                              Field
+                            </label>
                             <Field
                               name={`rules.${index}.field`}
-                              placeholder="Select field"
-                              as="select"
-                            >
-                                {() => selectOptionField(response, initialValue)}
-                            </Field>
-                          </div>
-                          <div className="col">
-                            <label htmlFor={`rules.${index}.rule`}>
-                              Rule
-                            </label>
+                              response={response}
+                              component={selectOptionField}
+                            />
+                          </RuleContainer>
+                          <RuleContainer>
+                            <label htmlFor={`rules.${index}.rule`}>Rule</label>
                             <Field
                               name={`rules.${index}.rule`}
-                              placeholder="Select the rule"
-                              as="select"
-                            >
-                                {() => selectOptionRule(initialValue)}
-                            </Field>
-                          </div>
-                          <div className="col">
-                            <label htmlFor={`rules.${index}.value`}>
-                              Value
-                            </label>
-                            <Field
-                              name={`rules.${index}.value`}
-                              placeholder="Select the field value"
-                              type="text"
+                              index={index}
+                              component={selectOptionRule}
                             />
-                          </div>
-                          <div className="col">
+                          </RuleContainer>
+                          {values.rules[index].rule !== "fieldValueDefined" && (
+                            <RuleContainer>
+                              <label htmlFor={`rules.${index}.value`}>
+                                Value
+                              </label>
+                              <Field
+                                name={`rules.${index}.value`}
+                                type="text"
+                                required
+                              />
+                            </RuleContainer>
+                          )}
+                          <RuleContainer>
                             <button
                               type="button"
                               className="secondary"
@@ -172,8 +149,8 @@ export const RulesForm: FC = () => {
                             >
                               X
                             </button>
-                          </div>
-                        </div>
+                          </RuleContainer>
+                        </RuleRow>
                       ))}
                     <button
                       type="button"
@@ -185,12 +162,15 @@ export const RulesForm: FC = () => {
                   </div>
                 )}
               </FieldArray>
-              <button type="submit" disabled={elementDisabled}>Save Rules</button>
+              <button type="submit" disabled={elementDisabled}>
+                Save Rules
+              </button>
             </Form>
           )}
         </Formik>
       </div>
-      <div>{JSON.stringify(rules)}</div>
+      <div>Saved Rules: {JSON.stringify(rules)}</div>
+      <div>initial Rules: {JSON.stringify(initialValue)}</div>
     </div>
   );
 };
